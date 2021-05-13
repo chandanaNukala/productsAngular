d1a68b00faedfaf2ee107af43437177bf2918498
Chandana <chandanareddynukala24@gmail.com>
2021-05-13 17:35:58 +0530

    routes&navigation

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import {Product} from '../models/product.model';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
+export class ProductDetailsComponent {
+  @Input() productObj:Product;
+  //custom event
+  @Output() myEvent=new EventEmitter();
+  sendProductToParent(title){
+    this.myEvent.emit(title);
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
-
 }
